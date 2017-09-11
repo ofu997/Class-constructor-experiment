d7b65afe9f36356e073a1b1821157b5fb8f9248a
oliver fu <ofu997@gmail.com>
2017-09-11 15:38:31 -0700

    Add console reading features

---
+++ Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// namespace ConsoleApp1
+
+public class Car
+{
+	// attributes
+	public string petName;
+	public int currSpeed; 
+
+	public void PrintState()
+	{
+		Console.WriteLine("name:{0} . speed:{1}",petName,currSpeed);
+	}
+
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+
+	// automatic name 
+class Program
 {
-	class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
+		// without explicit constructor
+		// this would use string variables for both variables
+		/*
+		Console.WriteLine("Write your car's name");
+		Car myCar = new Car();
+		myCar.petName = Console.ReadLine();
+
+		Console.WriteLine("Write your car's speed");
+		myCar.currSpeed = Console.ReadLine();
+		*/
+		
+		// WITH explicit constructor
+		
+		Console.WriteLine("Write your car's name");
+		string inputCarName=Console.ReadLine();
+		Console.WriteLine("Write your car's speed");
+		int inputCarSpeed = Int32.Parse(Console.ReadLine());
+		Car myCar = new Car( inputCarName, inputCarSpeed );
+		
+
+		// Console.WriteLine("Hello");
+		Console.WriteLine("Name: {0}. Speed: {1}", myCar.petName, myCar.currSpeed);
+		Console.ReadKey();
+	}
+
+	public class Car
+	{
+		public string petName;
+		public int currSpeed;
+		
+		 public Car (string name, int c)
 		{
+			this.petName = name + " with constructor";
+			this.currSpeed = c + 12; 
 		}
+		
 	}
 }
